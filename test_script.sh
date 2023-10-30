6536ee642cd71b1f9c698a55086b04ca52cc8568
duartepinto555 <duarte.pinto7337@gmail.com>
2023-10-30 13:45:31 +0000

    Finished test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+#!/bin/bash
 
-# Assuming your MPI program is compiled as read_matrix.out
 MPI_SCRIPT="read_matrix_speedup.c"
 MPI_EXECUTABLE="./a.out"
 
@@ -15,30 +15,51 @@ start_time=$(date +"%s")
 # Compile the C program
 mpicc "$MPI_SCRIPT" -o "$MPI_EXECUTABLE" -lm
 
-# Run the MPI program using mpirun
-echo "mpirun -n 4 \"$MPI_EXECUTABLE\" < \"$INPUT_DIR/input6\" > \"$OUTPUT_DIR/output6.txt\" 2>&1"
-mpirun -n 4 "$MPI_EXECUTABLE" < "$INPUT_DIR/input6" > "$OUTPUT_DIR/output6.txt" 2>&1
-echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
 
-start_time=$(date +"%s")
-echo "mpirun -n 4 \"$MPI_EXECUTABLE\" < \"$INPUT_DIR/input300\" > \"$OUTPUT_DIR/output300.txt\" 2>&1"
-mpirun -n 4 "$MPI_EXECUTABLE" < "$INPUT_DIR/input300" > "$OUTPUT_DIR/output300.txt" 2>&1
-echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
+# Number of process to iterate through
+numprocs=("1" "4" "9" "16" "25" "36")
 
-start_time=$(date +"%s")
-echo "mpirun -n 25 \"$MPI_EXECUTABLE\" < \"$INPUT_DIR/input600\" > \"$OUTPUT_DIR/output600.txt\" 2>&1"
-mpirun -n 25 "$MPI_EXECUTABLE" < "$INPUT_DIR/input600" > "$OUTPUT_DIR/output600.txt" 2>&1
-echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
+# Iterate through the array
+for i in "${numprocs[@]}"
+do
+    echo "############################################################"
+    echo ""
+    echo "                 TRYING WITH '$i' PROCESSES"
+    echo ""
+    echo "############################################################"
+    for file in "$INPUT_DIR"/*
+    do
+        echo "mpirun --hostfile hostfile -n $i \"$MPI_EXECUTABLE\" < \"$file\" > \"$OUTPUT_DIR/output_$(basename $file)-np_$i.txt\" 2>&1"
+        start_time=$(date +"%s")
+        mpirun --hostfile hostfile -n $i "$MPI_EXECUTABLE" < "$file" > "$OUTPUT_DIR/output_$(basename $file)-np_$i.txt" 2>&1
+        echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
+    done
+done
 
-start_time=$(date +"%s")
-echo "mpirun -n 25 \"$MPI_EXECUTABLE\" < \"$INPUT_DIR/input900\" > \"$OUTPUT_DIR/output900.txt\" 2>&1"
-mpirun -n 25 "$MPI_EXECUTABLE" < "$INPUT_DIR/input900" > "$OUTPUT_DIR/output900.txt" 2>&1
-echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
 
-start_time=$(date +"%s")
-echo "mpirun -n 36 \"$MPI_EXECUTABLE\" < \"$INPUT_DIR/input1200\" > \"$OUTPUT_DIR/output1200.txt\" 2>&1"
-mpirun -n 36 "$MPI_EXECUTABLE" < "$INPUT_DIR/input1200" > "$OUTPUT_DIR/output1200.txt" 2>&1
-echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
+# # Run the MPI program using mpirun
+# mpirun --hostfile hostfile -n 1 "$MPI_EXECUTABLE" < "$INPUT_DIR/input6" > "$OUTPUT_DIR/output6.txt" 2>&1
+# echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
 
-# Print a message indicating the completion of the run
-echo "Completed run. Output saved to $OUTPUT_DIR"
+# start_time=$(date +"%s")
+# echo "mpirun -n 1 \"$MPI_EXECUTABLE\" < \"$INPUT_DIR/input300\" > \"$OUTPUT_DIR/output300.txt\" 2>&1"
+# mpirun --hostfile hostfile -n 1 "$MPI_EXECUTABLE" < "$INPUT_DIR/input300" > "$OUTPUT_DIR/output300.txt" 2>&1
+# echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
+
+# start_time=$(date +"%s")
+# echo "mpirun -n 1 \"$MPI_EXECUTABLE\" < \"$INPUT_DIR/input600\" > \"$OUTPUT_DIR/output600.txt\" 2>&1"
+# mpirun --hostfile hostfile -n 1 "$MPI_EXECUTABLE" < "$INPUT_DIR/input600" > "$OUTPUT_DIR/output600.txt" 2>&1
+# echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
+
+# start_time=$(date +"%s")
+# echo "mpirun -n 1 \"$MPI_EXECUTABLE\" < \"$INPUT_DIR/input900\" > \"$OUTPUT_DIR/output900.txt\" 2>&1"
+# mpirun --hostfile hostfile -n 1 "$MPI_EXECUTABLE" < "$INPUT_DIR/input900" > "$OUTPUT_DIR/output900.txt" 2>&1
+# echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
+
+# start_time=$(date +"%s")
+# echo "mpirun -n 1 \"$MPI_EXECUTABLE\" < \"$INPUT_DIR/input1200\" > \"$OUTPUT_DIR/output1200.txt\" 2>&1"
+# mpirun --hostfile hostfile -n 1 "$MPI_EXECUTABLE" < "$INPUT_DIR/input1200" > "$OUTPUT_DIR/output1200.txt" 2>&1
+# echo "Elapsed Time: $(($(date +"%s") - start_time)) seconds"
+
+# # Print a message indicating the completion of the run
+# echo "Completed run. Output saved to $OUTPUT_DIR"
